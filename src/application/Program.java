c257696dd839d182ef12f5193d6237d830dce71a
hvgofernandes <hvgofernandes@gmail.com>
2025-02-26 22:07:59 -0300

    chore: Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ import java.util.Date;
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(4);
         System.out.println(seller);
     }
