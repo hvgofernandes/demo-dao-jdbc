a0dabc7d330cadae8bf14c1d8c87ba792bfb4927
hvgofernandes <hvgofernandes@gmail.com>
2025-02-27 19:58:13 -0300

    feat: Department update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -6,12 +6,16 @@ import model.entities.Department;
 
 public class Program2 {
     public static void main(String[] args) {
-
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
         System.out.println("=== TEST 1 department insert ===");
-        Department newDepartment = new Department(null, "Games");
+        Department newDepartment = new Department(null, "Toys");
         departmentDao.insert(newDepartment);
         System.out.println("Inserted! New department = " + newDepartment.getName());
         System.out.println(newDepartment);
+
+        System.out.println("=== TEST 2 department update ===");
+        newDepartment.setName("Videogames");
+        departmentDao.update(newDepartment);
+        System.out.println("Sucess! Department name updated.");
     }
 }
