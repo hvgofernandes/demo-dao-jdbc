a69250a983ee191b1beaf4be4929b8c95f13bc8d
hvgofernandes <hvgofernandes@gmail.com>
2025-02-26 22:33:15 -0300

    feat: findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+import java.util.List;
 
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
@@ -14,5 +15,12 @@ public class Program {
         System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(4);
         System.out.println(seller);
+
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
     }
 }
